f247375f65c11d64a2584bdfb52d6fde5d06ac9f
Ravi Yadav <ravi.yadav02@nagarro.com>
2021-09-23 18:42:35 +0530

    First Push

---
+++ firstproj/firstproj/Controllers/NewProjController.cs
@@ -0,0 +1,17 @@
+﻿using firstproj.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace firstproj.Controllers
+{
+    public class NewProjController : Controller
+    {
+        public IActionResult Name()
+        {
+            return View(new NewProjNameModel() {Name = "Ravi Yadav",ID ="1"});
+        }
+    }
+}
