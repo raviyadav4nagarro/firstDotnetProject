ff2e2e51b80b19299fa3c14e64ec7a677aea26b2
raviyadav02 <ravi.yadav02@nagarro.com>
2021-11-12 16:34:56 +0530

    Updates

---
+++ firstproj/firstproj/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace firstproj.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
